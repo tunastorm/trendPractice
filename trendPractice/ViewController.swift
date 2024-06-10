f2eb8f490d7785d3ad2df73b29870d8604983a5b
tunastorm <ycw921@naver.com>
2024-06-10 14:37:34 +0900

    Initial Commit

---
+++ trendPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trendPractice
+//
+//  Created by 유철원 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
